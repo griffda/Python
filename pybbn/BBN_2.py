b4e2bbac3dca647a1c263bdf25feb4a66b3aa576
griffda <griffiths1.thomas@gmail.com>
2022-08-18 17:01:23 +0100

    increased sample size to 50 and added histogram

--- pybbn/BBN_2.py
+++ pybbn/BBN_2.py
@@ -11,8 +11,8 @@ for the BOS model.
 This one is an update of BBN_1 and will attempt to dicretize the data into bins before being put through the BN. 
 """
 
-import networkx as nx # for drawing graphs
-import matplotlib.pyplot as plt # for drawing graphs
+import networkx as nx  # for drawing graphs
+import matplotlib.pyplot as plt  # for drawing graphs
 import pandas as pd
 from pybbn.graph.factory import Factory
 from pybbn.graph.dag import Bbn
@@ -24,96 +24,105 @@ from pybbn.pptc.inferencecontroller import InferenceController
 import numpy as np
 
 
-###Ingest data from csv fiel path and derive variables for use in the model into a dataframe
+# Ingest data from csv fiel path and derive variables for use in the model into a dataframe
 df = pd.read_csv('/Users/tomgriffiths/OneDrive - Imperial College London/Research/Python/gitlibraries/Python/output2.csv',
-                usecols = ['m','vf','KE'],
-                encoding=('utf-8')
-                )
-###we can print the dataframe and check that the above line has worked
-#print(df)
+                 usecols=['m', 'vf', 'KE'],
+                 encoding=('utf-8')
+                 )
+# we can print the dataframe and check that the above line has worked
+# print(df)
 
-###Now we can use the pd.cut function to dicretise the data into bins. 
-###These have been specified depending on the mass of the ball: very small, small etc. 
-df['m_bins']=pd.cut(x=df['m'], 
-                    bins=[0,0.5,1.0,1.5,2.0,2.5,3.0,3.5,4.0],  
-                    labels=["very very small", "very small", "small", "small/medium", "medium", "medium/large", "large", "very large"]
-                    )
+# Now we can use the pd.cut function to dicretise the data into bins.
+# These have been specified depending on the mass of the ball: very small, small etc.
+df['m_bins'] = pd.cut(x=df['m'],
+                      bins=[0, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0],
+                      labels=["very very small", "very small", "small", "small/medium", "medium", "medium/large", "large", "very large"]
+                      )
 
-df['vf_bins']=pd.cut(x=df['vf'], 
-                    bins=[0,4,8,12,16,20,24,28,33],  
-                    labels=["very very small", "very small", "small", "small/medium", "medium", "medium/large", "large", "very large"]
-                    )
+df['vf_bins'] = pd.cut(x=df['vf'],
+                       bins=[0, 4, 8, 12, 16, 20, 24, 28, 33],
+                       labels=["very very small", "very small", "small", "small/medium", "medium", "medium/large", "large", "very large"]
+                       )
 
-df['KE_bins']=pd.cut(x=df['KE'], 
-                    bins=[-7,-5.25,-3.5,-1.75,0,1.75,3.5,5.25,7],  
-                    labels=["very very small", "very small", "small", "small/medium", "medium", "medium/large", "large", "very large"]
-                    )
-
+df['KE_bins'] = pd.cut(x=df['KE'],
+                       bins=[-7, -5.25, -3.5, -1.75, 0, 1.75, 3.5, 5.25, 7],
+                       labels=["very very small", "very small", "small", "small/medium", "medium", "medium/large", "large", "very large"]
+                       )
 print(df)
 
-###we can print the dataframe and check that the above line has worked
+# we can print the dataframe and check that the above line has worked
 print(df['vf_bins'].value_counts(normalize=True).sort_index())
 
-##calculate probability distributions for the dataset
-def probs(data, child, parent1=None, parent2=None):
-    if parent1==None:
-        # Calculate probabilities
-        prob=pd.crosstab(data[child], 'Empty', margins=False, normalize='columns').sort_index().to_numpy().reshape(-1).tolist()
-    elif parent1!=None:
-            # Check if child node has 1 parent or 2 parents
-            if parent2==None:
-                # Calculate probabilities
-                prob=pd.crosstab(data[parent1],data[child], margins=False, normalize='index').sort_index().to_numpy().reshape(-1).tolist()
-            else:    
-                # Calculate probabilities
-                prob=pd.crosstab([data[parent1],data[parent2]],data[child], margins=False, normalize='index').sort_index().to_numpy().reshape(-1).tolist()
-    else: print("Error in Probability Frequency Calculations")
-    return prob 
+# calculate probability distributions for the dataset
 
-#create nodes for BBN  
+
+def probs(data, child, parent1=None, parent2=None):
+    if parent1 == None:
+        # Calculate probabilities
+        prob = pd.crosstab(data[child], 'Empty', margins=False, normalize='columns').sort_index(
+        ).to_numpy().reshape(-1).tolist()
+    elif parent1 != None:
+        # Check if child node has 1 parent or 2 parents
+        if parent2 == None:
+            # Calculate probabilities
+            prob = pd.crosstab(data[parent1], data[child], margins=False,
+                               normalize='index').sort_index().to_numpy().reshape(-1).tolist()
+        else:
+            # Calculate probabilities
+            prob = pd.crosstab([data[parent1], data[parent2]], data[child], margins=False,
+                               normalize='index').sort_index().to_numpy().reshape(-1).tolist()
+    else:
+        print("Error in Probability Frequency Calculations")
+    return prob
+
+
+# create nodes for BBN
 m = BbnNode(Variable(0, 'm', ["very very small", "very small", "small", "small/medium", "medium", "medium/large", "large", "very large"]), probs(df, child='m_bins'))
 vf = BbnNode(Variable(1, 'vf', ["very very small", "very small", "small", "small/medium", "medium", "medium/large", "large", "very large"]), probs(df, child='vf_bins', parent1='m_bins'))
-#KE = BbnNode(Variable(2, 'KE', ["very very small", "very small", "small", "small/medium", "medium", "medium/large", "large", "very large"]), probs(df, child='KE_bins', parent1='vf_bins'))
-#print(vf)
+KE = BbnNode(Variable(2, 'KE', ["very very small", "very small", "small", "small/medium", "medium", "medium/large", "large", "very large"]), probs(df, child='KE_bins', parent1='vf_bins'))
+# print(vf)
 
 
-#create network: 
+# create network:
 bbn = Bbn() \
     .add_node(m) \
     .add_node(vf) \
-    #.add_node(KE) \
-    #.add_edge(Edge(m, vf, EdgeType.DIRECTED)) \
-    #.add_edge(Edge(vf, KE, EdgeType.DIRECTED)) \
+    .add_node(KE) \
+    .add_edge(Edge(m, vf, EdgeType.DIRECTED)) \
+    .add_edge(Edge(vf, KE, EdgeType.DIRECTED))
 
-#create the BBN to a join tree 
-join_tree = InferenceController.apply(bbn)      
 
-## Create function for drawing the graph related to bbn. 
+# create the BBN to a join tree
+join_tree = InferenceController.apply(bbn)
 
-#Set node positions
+# Create function for drawing the graph related to bbn.
+
+# Set node positions
+
+
 def drawbn(bbn):
-        pos = {0: (-1, 2), 1: (-1, 0.5), 2: (1, 0.5), 3: (0, -1)}
+    pos = {0: (-1, 2), 1: (-1, 0.5), 2: (1, 0.5), 3: (0, -1)}
 
-        # Set options for graph looks
-        options = {
-            "font_size": 16,
-            "node_size": 4000,
-            "node_color": "white",
-            "edgecolors": "black",
-            "edge_color": "red",
-            "linewidths": 5,
-            "width": 5,}
-            
-        # Generate graph
-        n, d = bbn.to_nx_graph()
-        nx.draw(n, with_labels=True, labels=d, pos=pos, **options)
+    # Set options for graph looks
+    options = {
+        "font_size": 16,
+        "node_size": 4000,
+        "node_color": "white",
+        "edgecolors": "black",
+        "edge_color": "red",
+        "linewidths": 5,
+        "width": 5, }
 
-        # Update margins and print the graph
-        ax = plt.gca()
-        ax.margins(0.10)
-        plt.axis("off")
-        plt.show()
-        return plt
+    # Generate graph
+    n, d = bbn.to_nx_graph()
+    nx.draw(n, with_labels=True, labels=d, pos=pos, **options)
+
+    # Update margins and print the graph
+    ax = plt.gca()
+    ax.margins(0.10)
+    plt.axis("off")
+    plt.show()
+    return plt
 
 
 # Define a function for printing marginal probabilities
@@ -124,6 +133,8 @@ def print_probs():
         print("Values:")
         print(potential)
         print('----------------')
-        
+
 # Use the above function to print marginal probabilities
-#print_probs()
+print_probs()
+plt.hist(df["KE_bins"], bins =8)
+plt.show()
--- test_BOS_funcs.py
+++ test_BOS_funcs.py
@@ -13,26 +13,26 @@ from bos_sampling import ball_on_slope
 from bos_sampling import create_output_dataframe
 
 #generating one column of 5 samples for mass around value of 2 with SD of 1
-mass = lhs(1, samples=16, criterion=('center'))
+mass = lhs(1, samples=50, criterion=('center'))
 sample_masses = norm(loc=2, scale=1).ppf(mass)
 
 
 #generating 5 samples for radius around value of 1 with SD of 1
-radius = lhs(1, samples=16)
+radius = lhs(1, samples=50)
 sample_radii = norm(loc=1, scale=0.5).ppf(radius)
 
 #generating 5 samples for mu around value of 0.001 with SD of 0.00015
-fric_coeff = lhs(1, samples=16)
+fric_coeff = lhs(1, samples=50)
 sample_fric_coeffs = norm(loc=0.001, scale=0.00025).ppf(fric_coeff)
   
 
 #generating 5 samples for length around value of 10 with SD of 3.5
-length = lhs(1, samples=16)
+length = lhs(1, samples=50)
 sample_lengths = norm(loc=10, scale=3.5).ppf(length)
 
 
 #generating 5 samples for theta around value of 0.7 with SD of 0.15
-s_angle = lhs(1, samples=16)
+s_angle = lhs(1, samples=50)
 sample_s_angles = norm(loc=0.7, scale=0.15).ppf(s_angle)
 
 ##This runs a function using some parameters, and then returning a tuple containing sample array
