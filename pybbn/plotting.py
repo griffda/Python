d2983ca6218f9adf9c0ae78f93214be8ee854a1a
jhidalgosalaverri <jhsalaverri@us.es>
2023-05-15 16:41:15 +0200

    pybbn renamed to bnmodel

--- pybbn/plotting.py
+++
@@ -1,5 +0,0 @@
-from model_funcs import join_tree, get_obs_and_pred_posteriors, n_rows, n_cols, bin_edges_dict, prior_dict_xytrn, plot_posterior_probabilities
-
-obs_posteriors, predictedTargetPosteriors = get_obs_and_pred_posteriors(join_tree, "acceleration") ###obs_posteriors comes first. This is to match the order of the unpacking in the function call.
-
-plot_posterior_probabilities(n_rows, n_cols, bin_edges_dict, prior_dict_xytrn, obs_posteriors, plot=True) 
